5bef12872e2164397d2948383e086ba5c830ab6b
Chetak Pagare <chetakpagare@gmail.com>
2026-04-24 20:39:33 +0530

    Fix ASGI app startup order

--- backend/mystartup/asgi.py
+++ backend/mystartup/asgi.py
@@ -5,10 +5,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mystartup.settings")
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
+django_asgi_app = get_asgi_application()
+
 from core.ws_middleware import JWTAuthMiddleware
 from core.ws_routing import websocket_urlpatterns
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
