52f76ce477750934634aa54e8c646f22a1ff4bf8
Chetak Pagare <chetakpagare@gmail.com>
2026-06-15 13:40:42 +0530

    UI: Pin mobile chat composer to screen bottom and hide global Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,6 +35,7 @@ import Signup from "./pages/Signup";
 import SupportPage from "./pages/SupportPage";
 import TermsPage from "./pages/TermsPage";
 import Wallet from "./pages/Wallet";
+import useIsMobile from "./hooks/useIsMobile";
 
 const THEME_STORAGE_KEY = "sv-theme-preference";
 
@@ -186,6 +187,8 @@ function App() {
 
 function AppRoutes({ isAuth, setIsAuth, themeMode, toggleTheme }) {
   const location = useLocation();
+  const isMobile = useIsMobile();
+  const isChatPage = Boolean(location.pathname.match(/^\/groups\/[^/]+\/chat\/?$/));
   const [isRouteLoading, setIsRouteLoading] = useState(true);
   const [isSpotlightOpen, setIsSpotlightOpen] = useState(false);
   const [isScrollTopVisible, setIsScrollTopVisible] = useState(false);
@@ -245,7 +248,7 @@ function AppRoutes({ isAuth, setIsAuth, themeMode, toggleTheme }) {
         <span className="sv-route-loading-bar" />
       </div>
 
-      {isAuth ? (
+      {isAuth && !(isMobile && isChatPage) ? (
         <Navbar
           setIsAuth={setIsAuth}
           themeMode={themeMode}
