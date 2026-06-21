3754afcbaf239604519d18d2dcd8961a96974e12
Chetak Pagare <chetakpagare@gmail.com>
2026-06-21 14:41:38 +0530

    Fix ESLint no-useless-escape warning breaking Vercel build

--- frontend/src/pushSubscription.js
+++ frontend/src/pushSubscription.js
@@ -89,7 +89,7 @@ export async function unsubscribeFromPush() {
 // Utility function to convert base64 to Uint8Array
 function urlB64ToUint8Array(base64String) {
   const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding).replace(/\-/g, "+").replace(/_/g, "/");
+  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
 
   const rawData = window.atob(base64);
   const outputArray = new Uint8Array(rawData.length);
